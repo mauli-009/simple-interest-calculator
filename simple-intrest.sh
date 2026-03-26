db71ad0b2a3c6071ab957a86899c4cf5c444a984
Mauli Dudhat <151643077+mauli-009@users.noreply.github.com>
2026-03-26 13:54:27 +0530

    Create simple-intrest.sh

---
+++ simple-intrest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Simple Interest Calculator"
+
+echo "Enter Principal:"
+read p
+
+echo "Enter Rate of Interest:"
+read r
+
+echo "Enter Time:"
+read t
+
+si=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
+
+echo "Simple Interest is: $si"
